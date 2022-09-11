a50b674829bb1cfc1d9ca4f06e6e8754402b4d90
Pablo Hansen <vicky@pablos-air.ds.usfca.edu>
2022-09-11 10:10:12 -0700

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import InputButton from './Components/InputButton/InputButton';
+import { TouchableOpacity } from 'react-native';
+import { Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+      <TouchableOpacity style={ {height: 100}}>
+        <Image source={require('./rec.png')}  style={{justifyContent: 'center', alignself: 'center', textAlign: 'center', alignItems: 'center', flex:100, width: 100, height: 120}}/>
+    </TouchableOpacity>
+    
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  absoluteView: {
+      flex: 1,
+      position: 'absolute',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'transparent'
   },
+  btn: {
+  }
 });
---
+++ Components/InputButton/InputButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert, TouchableOpacity } from 'react-native';
+
+
+
+const InputButton = () => {
+    return (
+        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+
+        </View>);
+}
+
+export default InputButton;
+
+
