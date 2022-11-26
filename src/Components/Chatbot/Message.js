f1f92453d60e2b9756d63ae6219a3f16b4648011
pablojosecodes <pablojrhansen@gmail.com>
2022-11-25 21:52:20 -0800

    additional commenting

--- src/Components/Chatbot/Message.js
+++ src/Components/Chatbot/Message.js
@@ -3,32 +3,38 @@ import { Box, Center, InputLeftAddon, View } from "native-base";
 import { Image } from "react-native";
 import botty from "./botty.png";
 
+
+/**
+ * Message within the chatscreen
+ */
 const Message = (props) => {
+
+    //Side to align message to (sent vs incoming)
     const alignIt = props.right ? "right" : "left";
 
-    const distance = props.right ? 20: -100;
+    const distance = props.right ? 20 : -100;
 
     return (
         <>
-        <Image source ={require=("botty.png")} />
-        <Box
-            style={{
-                right: distance,
-                width: 300
-            }}
-            alignSelf="right"
-            p="6"
-            rounded="lg"
-            shadow={2}
-            _text={{
-                borderWidth: 2,
-                borderRadius: 4,
-                borderColor: "black", fontSize: 'md', fontWeight: 'bold', color: 'black'
-            }}
-        >
-            {props.text}
-        </Box>
-        {props.right ? <Image style={{height:40, width:40}}source ={botty} />: <></>}
+            <Image source={require = ("botty.png")} />
+            <Box
+                style={{
+                    right: distance,
+                    width: 300
+                }}
+                alignSelf="right"
+                p="6"
+                rounded="lg"
+                shadow={2}
+                _text={{
+                    borderWidth: 2,
+                    borderRadius: 4,
+                    borderColor: "black", fontSize: 'md', fontWeight: 'bold', color: 'black'
+                }}
+            >
+                {props.text}
+            </Box>
+            {props.right ? <Image style={{ height: 40, width: 40 }} source={botty} /> : <></>}
         </>
     );
 };
